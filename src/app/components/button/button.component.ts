5a1dd29c0e5b0c6a7fe133fb55fa5682c9469668
Matias <matias.varas@visma.com>
2023-06-18 22:07:03 -0300

    add property color in button component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -5,6 +5,17 @@ import { Component, Input } from '@angular/core';
   templateUrl: './button.component.html'
 })
 export class ButtonComponent {
-  @Input() buttonType: 'submit' | 'reset' | 'button' = 'button'
+  @Input() buttonType: 'submit' | 'reset' | 'button' = 'button';
+  @Input() color: string = "primary";
+
+  get colors(): string {
+    const colorsList : { [key: string]: string } = {
+      success: 'bg-success-700 hover:bg-success-800 focus:ring-success-300',
+      danger: 'bg-red-700 hover:bg-red-800 focus:ring-red-300',
+      primary: 'bg-primary-700 hover:bg-primary-800 focus:ring-primary-300',
+
+    }
+    return colorsList[this.color];
+  }
 
 }
