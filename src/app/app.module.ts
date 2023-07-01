fc800391e2f40fddf4f138fdf767dd7432440ec2
Matias <matias.varas@visma.com>
2023-07-01 12:46:24 -0300

    install cdk table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { OverlayModule } from '@angular/cdk/overlay';
 import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { DialogModule } from '@angular/cdk/dialog';
+import { CdkTableModule } from '@angular/cdk/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BoardComponent } from './pages/board/board.component';
 import { FormsModule } from '@angular/forms';
 import { TodoDialogComponent } from './components/todo-dialog/todo-dialog.component';
+import { TableComponent } from './components/table/table.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -24,7 +27,8 @@ import { TodoDialogComponent } from './components/todo-dialog/todo-dialog.compon
     BoardsComponent,
     NavbarComponent,
     BoardComponent,
-    TodoDialogComponent
+    TodoDialogComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +38,9 @@ import { TodoDialogComponent } from './components/todo-dialog/todo-dialog.compon
     CdkAccordionModule,
     DragDropModule,
     FormsModule,
-    DialogModule
+    DialogModule,
+    HttpClientModule,
+    CdkTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
